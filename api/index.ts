db800409250f4f328345a189005723a8fed19d94
Hassan <hassan@hashmove.com>
2024-03-30 06:11:15 +0500

    Fixes

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { bearerAuth } from 'hono/bearer-auth'
+// import { bearerAuth } from 'hono/bearer-auth'
 
 export const config = {
   runtime: 'edge',
@@ -8,13 +8,13 @@ export const config = {
 
 const app = new Hono().basePath('/api')
 
-app.get('/', (c) => c.json({ message: 'Hello Hono!' }))
+app.get('/hello', (c) => c.json({ message: 'Hello Hono!' }))
 
-const token = process.env.TOKEN ? process.env.TOKEN : '';
-app.use('/auth/*', bearerAuth({ token }))
+// const token = process.env.TOKEN ? process.env.TOKEN : '';
+// app.use('/auth/*', bearerAuth({ token }))
 
-app.get('auth/page', (c) => {
-  return c.text('you are authorized')
-})
+// app.get('auth/page', (c) => {
+//   return c.text('you are authorized')
+// })
 
 export default handle(app)
